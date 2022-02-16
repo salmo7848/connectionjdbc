0de743276f0993292e5321bdce4f40d39a76bd6f
Salmo Alves <salmo7848@outlook.com>
2022-02-16 13:55:19 -0300

    Utilização de JOption para caixas de dialogo

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -2,6 +2,8 @@ package aplication;
 
 import java.util.Date;
 
+import javax.swing.JOptionPane;
+
 import br.com.dadosSimples.DAO.DadosDAO;
 import br.com.dadosSimples.model.Complements;
 
@@ -10,16 +12,28 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Complements compls = new Complements();
-		DadosDAO dadosDao = new DadosDAO();		
+		DadosDAO dadosDao = new DadosDAO();
 		
-		compls.setNome("nome");
-		compls.setEmail("email");
-		compls.setTelefone("telefone");
+		
+		String nome = JOptionPane.showInputDialog("Informe o nome: ");
+		
+		String email = JOptionPane.showInputDialog("Informe o email: ");
+		
+		String telefone = JOptionPane.showInputDialog("Informe o telefone: ");
+	    JOptionPane.showMessageDialog(null, "Data de resgistro: "+new Date());
+			                     
+		
+		
+		compls.setNome(nome);
+		compls.setEmail(email);
+		compls.setTelefone(telefone);
 		compls.setDataCad(new Date());
 		
 		dadosDao.save(compls);
 		
-
+		
+		
+		
 	}
 
 }
